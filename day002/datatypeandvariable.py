4b65b771ac510e39876e35ee4a96247a66745720
maybexss <maybe_xss@163.com>
2017-10-27 15:32:52 +0800

    basic data type and variable study
    Signed-off-by: maybexss <maybe_xss@163.com>

---
+++ day002/datatypeandvariable.py
@@ -0,0 +1,75 @@
+str = "在python中能够直接处理的数据类新有以下几种："
+print(str)
+str = "整数--浮点数--字符串--布尔值--空值"
+print(str)
+str = "变量名必须是大小写英文、数字和_的组合，并且不能以数字开头"
+print(str)
+str = "常量是不能变的变量，在python中，通常用大写的变量名来表示常量"
+print(str)
+str = "如果字符串中既包含'又包含\"怎么办?可以用转义字符\\来标识,比如："
+print(str)
+str = 'I\\\'m \\\"OK\\\"!'
+print(str)
+str = '实际表示的字符串内容是：'
+print(str)
+str = 'I\'m \"OK\"!'
+print(str)
+str = '转义字符\\可以转义很多字符，比如\\n表示换行,\\t表示制表符,字符\\本身也要转义,所以\\\\表示的字符就是\\'
+print(str)
+str = '换行:\n制表符:\t字符\\本身'
+print(str)
+str = '如果字符串里面有很多字符都需要转义,就需要加很多\\,为了简化,python还允许用r\'\'表示\'\'内部的字符串不转义'
+print(str)
+str = '下面的字符串为利用了r\'\'的字符串:'
+print(str)
+str = r'如果字符串里面有很多字符都需要转义,就需要加很多\,为了简化,python还允许用r''表示''内部的字符串不转义'
+print(str)
+str = '如果字符串中内部有很多换行,用\\n卸载一行里不好阅读,为了简化,python允许用\'\'\'内容\'\'\'的格式表示多行内容'
+print(str)
+str = '下面的内容集利用了该方式的字符串:'
+print(str)
+str = '''line1
+line2
+line3'''
+print(str)
+str = '多行字符串还可以在前面加上r使用'
+print(str)
+str = r'''加\n了r\n
+的\\字符\t
+串\'
+.'''
+print(str)
+str = '布尔值Ture和False(注意首字母要大写)'
+print(str)
+str = '布尔值可以用and or 和not 运算'
+print(str)
+str = '空值是python里面的一个特殊值,用None表示'
+print(str)
+str = 'python中的除法/和//两种'
+print(str)
+str = '/的计算结果为浮点数,//的计算结果为取下整'
+print(str)
+str = '取模运算%取出后的余数'
+print(str)
+str = '例如: 10 / 3 ='
+print(str,10/3)
+str = '而10 // 3 ='
+print(str,10 // 3)
+str = '而10 % 3 ='
+print(str,10 % 3)
+
+str = '练习->输出下列变量的值：'
+print(str)
+str = 'n = 123'
+print(str)
+str = 'f = 456.789'
+print(str)
+str = 's1 = \'Hello, world!\''
+print(str)
+str = 's2 = \'Hello, \\\'maybexss\\\''
+print(str)
+str = 's3 = r\'Hello, \"bluer\"\''
+print(str)
+str = '''r\'\'\'Hello,
+Lisa!\'\'\''''
+print(str)
